02437ff018bcdce28cc327e8b3848deae5600168
Blake Hicks <bhicks@brunetgarcia.com>
2021-06-03 19:13:52 -0400

    Switched to yarn

--- src/modules/install.js
+++ src/modules/install.js
@@ -2,6 +2,6 @@ import exists from './exists.js';
 import run from './run.js';
 
 const install = dest =>
-	exists(`${dest}/package.json`) && run('npm', ['install', '--prefix', dest]);
+	exists(`${dest}/package.json`) && run('yarn', ['install', '--prefix', dest]);
 
 export default install;
