7c024f6a32994740424934aef40478f6cf29375d
Blake Hicks <bhicks@brunetgarcia.com>
2021-06-03 20:03:54 -0400

    Fix yarn dependency installation

--- src/modules/install.js
+++ src/modules/install.js
@@ -2,6 +2,6 @@ import exists from './exists.js';
 import run from './run.js';
 
 const install = dest =>
-	exists(`${dest}/package.json`) && run('yarn', ['install', '--prefix', dest]);
+	exists(`${dest}/package.json`) && run('yarn', ['install', '--cwd', dest]);
 
 export default install;
